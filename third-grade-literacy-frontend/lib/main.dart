5dc7eec675a49128996ab3d2d11604cd0cde052a
Nancy <nzhang301@gatech.edu>
2021-02-05 02:32:30 -0500

    Testing navigation

--- third-grade-literacy-frontend/lib/main.dart
+++ third-grade-literacy-frontend/lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'testing.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    title: '3rd Grade Literacy App',
+    home: MyApp()
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -17,7 +21,7 @@ class MyApp extends StatelessWidget {
           children: <Widget>[
             sideBar,
             Expanded(
-              child: mainSections,
+              child: mainSections(context),
             )
           ],
         )
@@ -53,133 +57,144 @@ Widget sideBar = Container(
   )
 );
 
-Widget mainSections = Container(
-  color: const Color(0xff6ec6d4),
-  child: Column(
-    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-    children: <Widget>[
-      Text(
-        'Word Structures',
-        style: TextStyle(
-          fontFamily: 'Comic',
-          fontSize: 30,
-        )
-      ),
-      Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Container(
-            color: const Color(0xff96e4a2),
-            child: Text('\n 1 \n Base Words and Endings \n ed ing \n',
+Widget mainSections(BuildContext context) {
+  return Container(
+      color: const Color(0xff6ec6d4),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Text(
+              'Word Structures',
               style: TextStyle(
                 fontFamily: 'Comic',
-                fontSize: 15
+                fontSize: 30,
+              )
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Test())
+                  );
+                },
+                child: Container(
+                    color: const Color(0xff96e4a2),
+                    child: Text('\n 1 \n Base Words and Endings \n ed ing \n',
+                      style: TextStyle(
+                          fontFamily: 'Comic',
+                          fontSize: 15
+                      ),
+                      textAlign: TextAlign.center,
+                    )
+                ),
               ),
-              textAlign: TextAlign.center,
-            )
+              Container(
+                  color: const Color(0xff66cf69),
+                  child: Text('\n 2 \n Base Words and Endings \n s es \n',
+                    style: TextStyle(
+                        fontFamily: 'Comic',
+                        fontSize: 15
+                    ),
+                    textAlign: TextAlign.center,
+                  )
+              ),
+              Container(
+                  color: const Color(0xfffdf885),
+                  child: Text('\n 3 \n Comparative \n Endings \n',
+                    style: TextStyle(
+                        fontFamily: 'Comic',
+                        fontSize: 15
+                    ),
+                    textAlign: TextAlign.center,
+                  )
+              ),
+            ],
           ),
-          Container(
-              color: const Color(0xff66cf69),
-              child: Text('\n 2 \n Base Words and Endings \n s es \n',
-                style: TextStyle(
-                    fontFamily: 'Comic',
-                    fontSize: 15
-                ),
-                textAlign: TextAlign.center,
-              )
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container(
+                  color: const Color(0xffbc8f75),
+                  child: Text('\n 4 \n Plurals \n  \n',
+                    style: TextStyle(
+                        fontFamily: 'Comic',
+                        fontSize: 15
+                    ),
+                    textAlign: TextAlign.center,
+                  )
+              ),
+              Container(
+                  color: const Color(0xfff3b2c8),
+                  child: Text('\n 5 \n Possessives \n  \n',
+                    style: TextStyle(
+                        fontFamily: 'Comic',
+                        fontSize: 15
+                    ),
+                    textAlign: TextAlign.center,
+                  )
+              ),
+              Container(
+                  color: const Color(0xfff3b98c),
+                  child: Text('\n 6 \n Contractions \n  \n',
+                    style: TextStyle(
+                        fontFamily: 'Comic',
+                        fontSize: 15
+                    ),
+                    textAlign: TextAlign.center,
+                  )
+              ),
+              Container(
+                  color: const Color(0xffb87cbe),
+                  child: Text('\n 7 \n Compound \n Words \n',
+                    style: TextStyle(
+                        fontFamily: 'Comic',
+                        fontSize: 15
+                    ),
+                    textAlign: TextAlign.center,
+                  )
+              ),
+            ],
           ),
-          Container(
-              color: const Color(0xfffdf885),
-              child: Text('\n 3 \n Comparative \n Endings \n',
-                style: TextStyle(
-                    fontFamily: 'Comic',
-                    fontSize: 15
-                ),
-                textAlign: TextAlign.center,
-              )
-          ),
-        ],
-      ),
-      Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Container(
-              color: const Color(0xffbc8f75),
-              child: Text('\n 4 \n Plurals \n  \n',
-                style: TextStyle(
-                    fontFamily: 'Comic',
-                    fontSize: 15
-                ),
-                textAlign: TextAlign.center,
-              )
-          ),
-          Container(
-              color: const Color(0xfff3b2c8),
-              child: Text('\n 5 \n Possessives \n  \n',
-                style: TextStyle(
-                    fontFamily: 'Comic',
-                    fontSize: 15
-                ),
-                textAlign: TextAlign.center,
-              )
-          ),
-          Container(
-              color: const Color(0xfff3b98c),
-              child: Text('\n 6 \n Contractions \n  \n',
-                style: TextStyle(
-                    fontFamily: 'Comic',
-                    fontSize: 15
-                ),
-                textAlign: TextAlign.center,
-              )
-          ),
-          Container(
-              color: const Color(0xffb87cbe),
-              child: Text('\n 7 \n Compound \n Words \n',
-                style: TextStyle(
-                    fontFamily: 'Comic',
-                    fontSize: 15
-                ),
-                textAlign: TextAlign.center,
-              )
-          ),
-        ],
-      ),
-      Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Container(
-              color: const Color(0xff6abad7),
-              child: Text('\n 8 \n Prefixes \n  \n',
-                style: TextStyle(
-                    fontFamily: 'Comic',
-                    fontSize: 15
-                ),
-                textAlign: TextAlign.center,
-              )
-          ),
-          Container(
-              color: const Color(0xfffcf351),
-              child: Text('\n 9 \n Suffixes \n  \n',
-                style: TextStyle(
-                    fontFamily: 'Comic',
-                    fontSize: 15
-                ),
-                textAlign: TextAlign.center,
-              )
-          ),
-          Container(
-              color: const Color(0xffee8640),
-              child: Text('\n 10 \n Syllables \n  \n',
-                style: TextStyle(
-                    fontFamily: 'Comic',
-                    fontSize: 15
-                ),
-                textAlign: TextAlign.center,
-              )
-          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container(
+                  color: const Color(0xff6abad7),
+                  child: Text('\n 8 \n Prefixes \n  \n',
+                    style: TextStyle(
+                        fontFamily: 'Comic',
+                        fontSize: 15
+                    ),
+                    textAlign: TextAlign.center,
+                  )
+              ),
+              Container(
+                  color: const Color(0xfffcf351),
+                  child: Text('\n 9 \n Suffixes \n  \n',
+                    style: TextStyle(
+                        fontFamily: 'Comic',
+                        fontSize: 15
+                    ),
+                    textAlign: TextAlign.center,
+                  )
+              ),
+              Container(
+                  color: const Color(0xffee8640),
+                  child: Text('\n 10 \n Syllables \n  \n',
+                    style: TextStyle(
+                        fontFamily: 'Comic',
+                        fontSize: 15
+                    ),
+                    textAlign: TextAlign.center,
+                  )
+              ),
+            ],
+          )
         ],
       )
-    ],
-  )
-);
+  );
+}
+
