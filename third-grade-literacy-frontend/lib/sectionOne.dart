c531ace08e9b3f3c9737fa376189df25a1f1d196
Nancy <nzhang301@gatech.edu>
2021-02-05 21:40:05 -0500

    Wrap subsection with Container to give background color

--- third-grade-literacy-frontend/lib/sectionOne.dart
+++ third-grade-literacy-frontend/lib/sectionOne.dart
@@ -60,78 +60,81 @@ Widget sideBar(BuildContext context) {
 }
 
 Widget subSections(BuildContext context) {
-  return Column(
-    mainAxisAlignment: MainAxisAlignment.spaceAround,
-    children: [
-      // title
-      Text('Base Words and Endings - ed, ing',
-        style: textStyle(Colors.black, 30)
-      ),
-      // subsections and clown
-      Row (
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          // 1.1 and 1.2
-          Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              // 1.1
-              Container(
-                width: screenHeight / 4,
-                height: screenHeight / 4,
-                child: Image.asset('assets/dropbox/Cropped/Icon_1.1.png',
-                  fit: BoxFit.contain,
-                )
-              ),
-              // effectively a spacer
-              Container(
-                height: screenHeight / 8,
-              ),
-              // 1.2
-              Container(
+  return Container(
+    color: const Color(0xffd5f0f7),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        // title
+        Text('Base Words and Endings - ed, ing',
+          style: textStyle(Colors.black, 30)
+        ),
+        // subsections and clown
+        Row (
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            // 1.1 and 1.2
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                // 1.1
+                Container(
+                  width: screenHeight / 4,
+                  height: screenHeight / 4,
+                  child: Image.asset('assets/dropbox/Cropped/Icon_1.1.png',
+                    fit: BoxFit.contain,
+                  )
+                ),
+                // effectively a spacer
+                Container(
+                  height: screenHeight / 8,
+                ),
+                // 1.2
+                Container(
                   width: screenHeight / 4,
                   height: screenHeight / 4,
                   child: Image.asset('assets/dropbox/Cropped/Icon_1.2.png',
                     fit: BoxFit.contain,
                   )
+                )
+              ],
+            ),
+            // Clown
+            Container(
+              height: screenHeight * 0.8,
+              child: Image.asset('assets/dropbox/Cropped/Clown_1.png',
+                fit: BoxFit.contain,
               )
-            ],
-          ),
-          // Clown
-          Container(
-            height: screenHeight * 0.8,
-            child: Image.asset('assets/dropbox/Cropped/Clown_1.png',
-              fit: BoxFit.contain,
-            )
-          ),
-          // 1.3 and 1.4
-          Column(
-            children: [
-              // 1.3
-              Container(
+            ),
+            // 1.3 and 1.4
+            Column(
+              children: [
+                // 1.3
+                Container(
                   width: screenHeight / 4,
                   height: screenHeight / 4,
                   child: Image.asset('assets/dropbox/Cropped/Icon_1.3.png',
                     fit: BoxFit.contain,
                   )
-              ),
-              // effectively a spacer
-              Container(
-                height: screenHeight / 8,
-              ),
-              // 1.4
-              Container(
+                ),
+                // effectively a spacer
+                Container(
+                  height: screenHeight / 8,
+                ),
+                // 1.4
+                Container(
                   width: screenHeight / 4,
                   height: screenHeight / 4,
                   child: Image.asset('assets/dropbox/Cropped/Icon_1.4.png',
                     fit: BoxFit.contain,
                   )
-              )
-            ],
-          )
-        ],
-      )
-    ],
+                )
+              ],
+            )
+          ],
+        )
+      ],
+    )
   );
 }
 
