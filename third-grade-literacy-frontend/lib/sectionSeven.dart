6bacdec64b402ff2de2752aee9cf0b80ef9cf9ad
vsilva021900 <vsilva9@gatech.edu>
2021-02-07 04:24:50 -0500

    Finished Section 8 and started Section 7

--- third-grade-literacy-frontend/lib/sectionSeven.dart
+++ third-grade-literacy-frontend/lib/sectionSeven.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'testing.dart';
 
 class MainSeven extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    screenHeight = MediaQuery.of(context).size.height;
+    screenWidth = MediaQuery.of(context).size.width;
     return MaterialApp(
         home: Material(
             child: Row(
@@ -57,15 +60,100 @@ Widget sideBar(BuildContext context) {
 }
 
 Widget subSections(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/dropbox/wordStructuresMenu/sectionSevenMenu.png"),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: null /* add child content here */,
-      ),
-    );
+  return Container(
+      color: const Color(0xffffffff),
+      child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                PageRouteBuilder(
+                                    pageBuilder: (context, _, __) => Test(),
+                                    transitionDuration: Duration(seconds: 0)
+                                )
+                            );
+                          },
+                          child: Container(
+                              height: screenHeight / 3.75,
+                              child: Image.asset('assets/dropbox/Cropped/Icon_7.1.png',
+                                fit: BoxFit.fitHeight,
+                              )
+                          )
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text('compound',
+                              style: textStyle(Colors.black, 30)
+                          ),
+                          Text('words',
+                              style: textStyle(Colors.black, 30)
+                          ),
+                        ],
+                      ),
+                      GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                PageRouteBuilder(
+                                    pageBuilder: (context, _, __) => Test(),
+                                    transitionDuration: Duration(seconds: 0)
+                                )
+                            );
+                          },
+                          child: Container(
+                              height: screenHeight / 3.75,
+                              child: Image.asset('assets/dropbox/Cropped/Icon_7.2.png',
+                                fit: BoxFit.fitHeight,
+                              )
+                          )
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                PageRouteBuilder(
+                                    pageBuilder: (context, _, __) => Test(),
+                                    transitionDuration: Duration(seconds: 0)
+                                )
+                            );
+                          },
+                          child: Container(
+                              height: screenHeight / 3.75,
+                              child: Image.asset('assets/dropbox/Cropped/Icon_7.3.png',
+                                fit: BoxFit.fitHeight,
+                              )
+                          )
+                      ),
+                    ],
+                  ),
+                ]
+            )
+          ]
+      )
+  );
+}
+
+double screenHeight, screenWidth;
+TextStyle textStyle(Color col, double size) {
+  return TextStyle(
+    color: col,
+    fontFamily: 'Comic',
+    fontSize: size,
+  );
 }
