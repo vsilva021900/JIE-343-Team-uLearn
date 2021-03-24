ff17de9f03fad4ce63f5adde4c9b75003286bfed
Nancy <nzhang301@gatech.edu>
2021-03-24 01:36:16 -0400

    Lesson for 9.3

---
+++ third-grade-literacy-frontend/lib/nine/NinePointThreeLesson.dart
@@ -0,0 +1,265 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:hearatale_literacy_app/WordStructures.dart';
+import 'package:hearatale_literacy_app/nine/ScoreMenuNine.dart';
+import 'package:hearatale_literacy_app/nine/quiz/QuizNinePointThree.dart';
+
+
+class NinePointThreeLesson extends StatefulWidget {
+  @override
+  NinePointThree createState() => NinePointThree();
+}
+
+class NinePointThree extends State<NinePointThreeLesson> {
+  var pictures = [
+    "assets/dropbox/SectionNine/NinePointThree/fearless.png",
+    "assets/dropbox/SectionNine/NinePointThree/clueless.png",
+    "assets/dropbox/SectionNine/NinePointThree/boneless.png",
+    "assets/dropbox/SectionNine/NinePointThree/harmless.png",
+    "assets/dropbox/SectionNine/NinePointThree/colorless.png",
+    "assets/dropbox/SectionNine/NinePointThree/tasteless.jpg"
+  ];
+  var words = [
+    ["fear", "less", "fearless"],
+    ["clue", "less", "clueless"],
+    ["bone", "less", "boneless"],
+    ["harm", "less", "harmless"],
+    ["color", "less", "colorless"],
+    ["taste", "less", "tasteless"]
+  ];
+  var wordsAudio = [
+    "dropbox/SectionNine/NinePointThree/fear_less_fearless.mp3",
+    "dropbox/SectionNine/NinePointThree/clue_less_clueless.mp3",
+    "dropbox/SectionNine/NinePointThree/bone_less_boneless.mp3",
+    "dropbox/SectionNine/NinePointThree/harm_less_harmless.mp3",
+    "dropbox/SectionNine/NinePointThree/color_less_colorless.mp3",
+    "dropbox/SectionNine/NinePointThree/taste_less_tasteless.mp3"
+  ];
+
+  int tracker = 0;
+  bool marker = true;
+
+  AudioCache audioCache = new AudioCache();
+  AudioPlayer audioPlayer = new AudioPlayer();
+  String sentenceAudio = "dropbox/SectionNine/NinePointThree/#9.3_suffixLESSmeansWithout.mp3";
+
+  @override
+  Widget build(BuildContext context) {
+    screenWidth = MediaQuery.of(context).size.width;
+    screenHeight = MediaQuery.of(context).size.height;
+
+    if (marker) {
+      marker = false;
+      playSentenceAudio();
+    }
+
+    return MaterialApp(
+        home: Material(
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                sideBarWithReplay(context),
+                Expanded(
+                    child: sub(context)
+                )
+              ],
+            )
+        )
+    );
+  }
+
+  Widget sideBarWithReplay(BuildContext context) {
+    return Container(
+        color: const Color(0xffc4e8e6),
+        child: Column(
+            children: <Widget>[
+              Material(
+                  color: const Color(0xffc4e8e6),
+                  child: IconButton(
+                    icon: Image.asset('assets/placeholder_back_button.png'),
+                    onPressed: () {
+                      stopAudio();
+                      Navigator.pop(context);
+                    },
+                  )
+              ),
+              Material(
+                  color: const Color(0xffc4e8e6),
+                  child: IconButton(
+                    icon: Image.asset('assets/placeholder_home_button.png'),
+                    onPressed: () {
+                      stopAudio();
+                      Navigator.pushAndRemoveUntil(context,
+                          PageRouteBuilder(
+                              pageBuilder: (context, _, __) => MyApp(),
+                              transitionDuration: Duration(seconds: 0)
+                          ), (route) => false);
+                    },
+                  )
+              ),
+              Spacer(flex: 5),
+              Material(
+                  color: const Color(0xffc4e8e6),
+                  child: IconButton(
+                      icon: Image.asset('assets/placeholder_quiz_button.png'),
+                      onPressed: () {
+                        stopAudio();
+                        Navigator.push(
+                            context,
+                            PageRouteBuilder(
+                                pageBuilder: (context, _, __) => QuizNinePointThree(),
+                                transitionDuration: Duration(seconds: 0)
+                            )
+                        );
+                      }
+                  )
+              ),
+              Material(
+                  color: const Color(0xffc4e8e6),
+                  child: IconButton(
+                      icon: Image.asset('assets/placeholder_replay_button.png'),
+                      onPressed: () {
+                        playWordsAudio();
+                      }
+                  )
+              ),
+              Material(
+                  color: const Color(0xffc4e8e6),
+                  child: IconButton(
+                    icon: Image.asset('assets/star_button.png'),
+                    onPressed: () {
+                      stopAudio();
+                      Navigator.push(
+                          context,
+                          PageRouteBuilder(
+                              pageBuilder: (context, _, __) => ScoreNine(),
+                              transitionDuration: Duration(seconds: 0)
+                          )
+                      );
+                    },
+                  )
+              ),
+              Material(
+                  color: const Color(0xffc4e8e6),
+                  child: IconButton(
+                      icon: Image.asset('assets/placeholder_piggy_button.png'),
+                      onPressed: () {
+                        stopAudio();
+                      }
+                  )
+              ),
+            ]
+        )
+    );
+  }
+
+  Widget sub(BuildContext context) {
+    return Container(
+        color: const Color(0xFFFFFF),
+        child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // can probably simplify with RichText
+                  Text('The suffix ',
+                      style: textStyle(Colors.black, screenWidth / 24)
+                  ),
+                  Text('less ',
+                      style: textStyle(Colors.green, screenWidth / 24)
+                  ),
+                  Text('means ',
+                      style: textStyle(Colors.black, screenWidth / 24)
+                  ),
+                  Text('without',
+                      style: textStyle(Colors.red, screenWidth / 24)
+                  ),
+                  Text('.',
+                      style: textStyle(Colors.black, screenWidth / 24)
+                  )
+                ],
+              ),
+              Text('',
+                  style: textStyle(Colors.black, screenWidth / 24)
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                    height: screenHeight * 0.6,
+                    child: Transform.scale(
+                      scale: 1,
+                      child: IconButton(
+                        icon: Image.asset('assets/placeholder_back_button.png'),
+                        onPressed: () {
+                          setState(() { tracker = (tracker == 0)? pictures.length - 1 : tracker - 1;});
+                          playWordsAudio();
+                        },
+                      ),
+                    ),
+                  ),
+                  Container(
+                      height: screenHeight * 0.6,
+                      child: Image.asset(pictures[tracker]),
+                      width: 200
+                  ),
+                  Container(
+                    height: screenHeight * 0.6,
+                    child: Transform.scale(
+                      scale: 1,
+                      child: IconButton(
+                        icon: Image.asset('assets/placeholder_back_button_reversed.png'),
+                        onPressed: () {
+                          setState(() { tracker = (tracker == pictures.length - 1)? 0 : tracker + 1;});
+                          playWordsAudio();
+                        },
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text(words[tracker][0],
+                      style: textStyle(Colors.black, 30)
+                  ),
+                  Text(words[tracker][1],
+                      style: textStyle(Colors.black, 30)
+                  ),
+                  Text(words[tracker][2],
+                      style: textStyle(Colors.black, 30)
+                  )
+                ],
+              )
+            ]
+        )
+    );
+
+
+  }
+
+  playSentenceAudio() async {
+    stopAudio();
+    audioPlayer = await audioCache.play(sentenceAudio);
+  }
+
+  playWordsAudio() async {
+    stopAudio();
+    audioPlayer = await audioCache.play(wordsAudio[tracker]);
+  }
+
+  stopAudio() {
+    audioPlayer.stop();
+  }
+}
+
+double screenHeight, screenWidth;
+TextStyle textStyle(Color col, double size) {
+  return TextStyle(
+    color: col,
+    fontFamily: 'Comic',
+    fontSize: size,
+  );
+}
